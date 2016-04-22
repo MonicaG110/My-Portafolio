e8666f52f373ca8f0a767e0207c443b6810713b1
Monica <monicamillerg@gmail.com>
2016-04-22 12:18:12 -0700

    progress in menu

---
+++ main.js
@@ -0,0 +1,24 @@
+/*$(document).ready(function(){
+
+    $('<select>').appendTo(".navBar nav div");
+
+    /*$("<option>", {
+     "selected": "selected",
+     "value"   : "",
+     "text"    : "Go to..."
+    }).appendTo(".section-nav select");
+
+
+   $(".section-nav a").each(function() {
+    var el = $(this);
+    $("<option>", {
+     "value"   : el.attr("href"),
+     "text"    : el.text()
+    }).appendTo(".navBar nav select");
+  });
+
+   $(".section-nav select").change(function() {
+  window.location = $(this).find("option:selected").val();
+});
+
+});*/
